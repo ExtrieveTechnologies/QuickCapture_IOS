67cc231db803f77ab52ed24c574ed442c396632e
Amalkarunakaran <amal.karunakaran@extrieve.com>
2023-06-28 12:31:49 +0530

    sample obj-c app updates
    updated sample app with proper comments

--- Objective-C_sample/TestObjcApp/ViewController.m
+++ Objective-C_sample/TestObjcApp/ViewController.m
@@ -5,44 +5,44 @@
 //  Created by Extrieve Technologies Pvt Ltd on 14/06/23.
 //
 
+///Framework contains two Header file which is Necessary to import
+/// QuickCaptureSDK-Swift.h and QuickCaptureFW.h
+/// Inside the Framework Package you will find under the Header Folder
+///
+/// import "<Path to Framework>/QuickCaptureSDK.framework/Headers/QuickCaptureSDK-Swift.h"
+/// import "<Path to Framework>/QuickCaptureSDK.framework/Headers/QuickCaptureFW.h"
+
+
 #import "ViewController.h"
 
 
 #ifdef RELEASE
-//For CameraHelper Support
+/// Necessary For CameraHelper Support
 #import "../../Build/Products/Release-iphoneos/QuickCaptureSDK.framework/Headers/QuickCaptureSDK-Swift.h"
-//For ImgHelperSupport
+/// Necessary For ImgHelper Support
 #import "../../Build/Products/Release-iphoneos/QuickCaptureSDK.framework/Headers/QuickCaptureFW.h"
 #else
-//For CameraHelper Support
+/// Necessary For CameraHelper Support
 #import "../../Build/Products/Debug-iphoneos/QuickCaptureSDK.framework/Headers/QuickCaptureSDK-Swift.h"
-//For ImgHelperSupport
+/// Necessary For ImgHelper Support
 #import "../../Build/Products/Debug-iphoneos/QuickCaptureSDK.framework/Headers/QuickCaptureFW.h"
 
 #endif
 
+
 typedef void (^CaptureCallbackBlock)(NSArray<NSString *> * _Nonnull imageArray);
-
-
-
 
 @interface ViewController ()
 @property (nonatomic, copy, nullable) CaptureCallbackBlock captureCallback;
-
+@property __block NSArray<NSString *>  *CapturedImages;
 @end
 
 @implementation ViewController
 
 
-- (void) CallbackForCaptureCB:(NSArray<NSString *> *) imageArray
-{
-    return;
-}
 
 
 - (IBAction)btnLaunchCameraTap:(id)sender {
-
-
 
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     
@@ -54,63 +54,108 @@ typedef void (^CaptureCallbackBlock)(NSArray<NSString *> * _Nonnull imageArray);
     }
     
     
+    ///Some Configurations before starting the Camera Capture
+    ///The camera Capture session will behave according to these settings
     NSURL *workingDirUrl = [url URLByAppendingPathComponent:@"QuickCaptureDemoWorking1"];
     CaptureSupport.OutputPath = [workingDirUrl absoluteString];
     CaptureSupport.CaptureSound = true;
+    CaptureSupport.ColorMode = ColorModesRGB;
+    CaptureSupport.CameraToggle = 2;
+    CaptureSupport.EnableFlash = false;
 
     
-    NSString *documentDir = [paths firstObject];
+    NSString *workingDir = [paths firstObject];
     
     
-    //Handling the captured images in this callback function
+    ///This callback will be Required for CameraHelper.StartCapture as an argument.
+    ///This will be called after the Capture Session is closed
     self.captureCallback = ^(NSArray<NSString *> * _Nonnull imageArray) {
         if (imageArray.count == 0) {
             NSLog(@"Error: no images found");
         }
         else{
-            NSLog(@"%lu", (unsigned long)imageArray.count);
-            BOOL isExist = [NSFileManager.defaultManager fileExistsAtPath:imageArray[0]];
             
-            ImgHelper *img = [ImgHelper GetInstance];
-            
-            NSURL *workingDirURL = [NSURL URLWithString:CaptureSupport.OutputPath];
-            if (!workingDirURL) {
-            return;
-            }
-
-            NSFileManager *manager = [NSFileManager defaultManager];
-            BOOL isDirectory = NO;
-            if (![manager fileExistsAtPath:workingDirURL.path isDirectory:&isDirectory] || !isDirectory) {
-            NSError *error = nil;
-            [manager createDirectoryAtPath:workingDirURL.path withIntermediateDirectories:YES attributes:nil error:&error];
-            if (error) {
-            NSLog(@"Unable to create output directory");
-            }
-            }
-
-            NSURL *outputTiffURL = [workingDirURL URLByAppendingPathComponent:@"OutputPDF.JPEG"];
-            NSError * _Nullable  err;
-            
-            [img SetDpi:DPI_100];
-            [img setPageLayout:LayoutTypeA0];
-
-            UIImage* thumb = [img CompressAndGetThumbnail:imageArray[0] outputURI:outputTiffURL.absoluteString err:&err];
-            //UIImage* thumb = [img CompressAndGetThumbnailWithInputURI:imageArray[0] outputURI:outputTiffURL.absoluteString error:&err];
-            NSLog(@"%@",err.description);
-            NSLog(@"%@",err.localizedDescription);
-            
-            int ret = [img createPdfFileFromJpegArray:workingDirUrl.path :imageArray :outputTiffURL.path :200 :200 :40];
-            
-            NSLog(@"%d",ret);
+            ///imageArray is the Argument where you get the Array od Strings
+            ///these strings are the path of captured image saved in WorkingDirectory temporarily
+            ///Note: Don't forget to delete image files after completion of task
+            self->_CapturedImages = imageArray;
         }
     };
     
+    ///Instanciating the CameraHelper Class to use its properties
     CameraHelper *cameraHelperObj = [[CameraHelper alloc] init];
 
         
-    //Start Capturing the Images from Camera using Framework
-    [cameraHelperObj StartCapture:self :documentDir : self.captureCallback];
+    ///Start Capturing the Images from Camera using Framework
+    ///It will save all the Captured images
+    [cameraHelperObj StartCapture:self :workingDir : self.captureCallback];
 }
+
+
+
+///This button tap Action is providing the Usage of ImageHelper Class Properties
+- (IBAction)btnActionTap:(id)sender{
+    
+    
+    NSLog(@"%lu", (unsigned long)self->_CapturedImages.count);
+    BOOL isExist = [NSFileManager.defaultManager fileExistsAtPath:self->_CapturedImages[0]];
+    
+    
+    NSURL *workingDirURL = [NSURL URLWithString:CaptureSupport.OutputPath];
+    if (!workingDirURL) {
+    return;
+    }
+
+    NSFileManager *manager = [NSFileManager defaultManager];
+    BOOL isDirectory = NO;
+    if (![manager fileExistsAtPath:workingDirURL.path isDirectory:&isDirectory] || !isDirectory) {
+    NSError *error = nil;
+    [manager createDirectoryAtPath:workingDirURL.path withIntermediateDirectories:YES attributes:nil error:&error];
+    if (error) {
+    NSLog(@"Unable to create output directory");
+    }
+    }
+
+    NSURL *outputTiffURL = [workingDirURL URLByAppendingPathComponent:@"OutputPDF.JPEG"];
+    NSError * _Nullable  err;
+    
+    
+    ///Instanciating the ImgHelper class using GetInstance Method
+    ImgHelper *img = [ImgHelper GetInstance];
+    
+    ///Setting the DPI as 100 using SetDpi method which takes enum value as an argument
+    [img SetDpi:DPI_100];
+    
+    ///Setting the PageLayout as A4 using setPageLayout method which takes enum value as an argument
+    [img setPageLayout:LayoutTypeA4];
+
+    
+    ///CompressAndGetThumbnail : This method Returns an UIImage object which is THumbnail of the image path passed as an argument
+    ///It saves the compressed image on outputURI path
+    ///Last argument "err" is for any excceptions occurs
+    ///Note: Here we are providing the AbsoluteString as it take the FullPath to save the image
+    UIImage* thumb = [img CompressAndGetThumbnail:self->_CapturedImages[0] outputURI:outputTiffURL.absoluteString err:&err];
+    NSLog(@"%@",err.description);
+    NSLog(@"%@",err.localizedDescription);
+    
+    ///Getting the ImageQuality
+    int quality = [img getJPEGQuality];
+    
+    ///createPdfFileFromJpegArray : This method will create a PDF out of Images provided by the NSArray of NSString saves the pdf file in outputTiffURL provided as String
+    ///First argument is working directory it should be readable and writeable directory where this function saves some temporary data then removes after completion
+    ///Return Integer value as 0 means TRUE or success and vice versa
+    int ret = [img createPdfFileFromJpegArray:workingDirURL.path :self->_CapturedImages :outputTiffURL.path :DPI_200 :DPI_200 :quality];
+    
+    NSLog(@"%d",ret);
+    
+    
+    
+    
+}
+
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
